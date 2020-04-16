540174eabc06ec2c1f0850249adc37009fa3838b
Elijah <rahulsharma240301@gmail.com>
2020-04-16 13:30:51 +0530

    Initial commit. Dev Setup

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+
+async function getResults(query) {
+    try{
+
+        const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${query}`);
+        console.log(res.data.recipes)
+    } catch(error) {
+        alert(error);        
+    }
+}
+getResults('Pizza');
